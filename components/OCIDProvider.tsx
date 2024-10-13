f71cd167513314d8bad3fa7d575ae5aef00e5fc7
Asharib <asharibarain4@gmail.com>
2024-10-14 04:01:54 +0500

    update

--- components/OCIDProvider.tsx
+++ components/OCIDProvider.tsx
@@ -8,13 +8,12 @@ interface OCIDProviderProps {
 }
 
 const opts = {
-  redirectUri: "http://localhost:3000/redirect",
+  redirectUri: "https://ai.eduhub.dev/redirect",
+  referralCode: "ASHARIB",
 };
 
 const OCIDProvider: FC<OCIDProviderProps> = ({ children }) => (
-  <OCConnect opts={opts} sandboxMode={true}>
-    {children}
-  </OCConnect>
+  <OCConnect opts={opts}>{children}</OCConnect>
 );
 
 export default OCIDProvider;
